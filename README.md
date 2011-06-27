a6b19a44d612450af7f1f02508b5b451b8995378
Luke Daley <ld@ldaley.com>
2011-06-27 23:47:09 +1000

    Updated to latest Geb.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ This is a simple example project for using Geb with Maven.
 Just check it out and run…
 
     mvn test
+
+Please see the `src/test/resources/GebConfig.groovy` file for how to run the tests with different browsers.
---
+++ src/test/resources/GebConfig.groovy
@@ -0,0 +1,33 @@
+/*
+	This is the Geb configuration file.
+	
+	See: http://www.gebish.org/manual/current/configuration.html
+*/
+
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.chrome.ChromeDriver
+
+// Use htmlunit as the default
+// See: http://code.google.com/p/selenium/wiki/HtmlUnitDriver
+driver = { 
+	def driver = new HtmlUnitDriver()
+	driver.javascriptEnabled = true
+	driver
+}
+
+environments {
+	
+	// run as “mvn -Dgeb.env=chrome test”
+	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
+	chrome {
+		driver = { new ChromeDriver() }
+	}
+	
+	// run as “mvn -Dgeb.env=firefox test”
+	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
+	firefox {
+		driver = { new FirefoxDriver() }
+	}
+
+}
