f781f973315ba666ba048b0e3dc2a2cb73cc449b
Luke Daley <ld@ldaley.com>
2012-11-14 18:00:55 +0000

    Update to latest Geb.

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -4,17 +4,11 @@
 	See: http://www.gebish.org/manual/current/configuration.html
 */
 
-import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.chrome.ChromeDriver
 
-// Use htmlunit as the default
 // See: http://code.google.com/p/selenium/wiki/HtmlUnitDriver
-driver = { 
-	def driver = new HtmlUnitDriver()
-	driver.javascriptEnabled = true
-	driver
-}
+driver = { new FirefoxDriver() }
 
 environments {
 	
@@ -22,12 +16,6 @@ environments {
 	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
 	chrome {
 		driver = { new ChromeDriver() }
-	}
-	
-	// run as “mvn -Dgeb.env=firefox test”
-	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
-	firefox {
-		driver = { new FirefoxDriver() }
 	}
 
 }
