e7b4953ad3afe496fc15ab75a88d3e325a3fb7a0
Marcin Erdmann <erdi84@gmail.com>
2014-08-17 17:30:42 +0100

    Update dependency versions, only use firefox driver as it does not require any special additional executables

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -5,17 +5,5 @@
 */
 
 import org.openqa.selenium.firefox.FirefoxDriver
-import org.openqa.selenium.chrome.ChromeDriver
 
-// See: http://code.google.com/p/selenium/wiki/HtmlUnitDriver
 driver = { new FirefoxDriver() }
-
-environments {
-	
-	// run as “mvn -Dgeb.env=chrome test”
-	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
-	chrome {
-		driver = { new ChromeDriver() }
-	}
-
-}
