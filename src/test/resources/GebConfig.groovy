0c3def1a477bc85bd8655283ab163eeda85824ef
Jonny Carter <jcarter@adaptavist.com>
2025-09-12 19:45:49 -0500

    Update Geb Version
    Also update JUnit, Surefire, GMavenPlus, and remove needless
    chromedriver config.

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,40 +1,6 @@
 /*
 	This is the Geb configuration file.
-	
+
 	See: http://www.gebish.org/manual/current/configuration.html
 */
-import org.openqa.selenium.chrome.ChromeDriver
-import org.openqa.selenium.chrome.ChromeDriverService
-import org.openqa.selenium.os.ExecutableFinder
-
-import static org.apache.commons.lang3.SystemUtils.IS_OS_LINUX
-import static org.apache.commons.lang3.SystemUtils.IS_OS_MAC
-import static org.apache.commons.lang3.SystemUtils.IS_OS_WINDOWS
-
-File findDriverExecutable() {
-    def defaultExecutable = new ExecutableFinder().find("chromedriver")
-    if (defaultExecutable) {
-        new File(defaultExecutable)
-    } else {
-        new File("drivers").listFiles().findAll {
-            !it.name.endsWith(".version")
-        }.find {
-            if (IS_OS_LINUX) {
-                it.name.contains("linux")
-            } else if (IS_OS_MAC) {
-                it.name.contains("mac")
-            } else if (IS_OS_WINDOWS) {
-                it.name.contains("windows")
-            }
-        }
-    }
-}
-
-driver = {
-    ChromeDriverService.Builder serviceBuilder = new ChromeDriverService.Builder()
-        .usingAnyFreePort()
-        .usingDriverExecutable(findDriverExecutable())
-    new ChromeDriver(serviceBuilder.build())
-}
-
 baseUrl = "http://gebish.org"
